d7e88755af9c96078972aaaffec7ab3e9044f374
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2020-01-02 02:51:14 -0600

    A few tweaks to the code.

--- planning/clean_assignments.R
+++ planning/clean_assignments.R
@@ -1,30 +1,72 @@
-old_wd = getwd()
-setwd(rprojroot::find_root(".semestr.here"))
+clean_assignment_files <- function(verbose = TRUE, dry_run = TRUE) {
 
-gen_dirs <- c("assignment", "homework_solutions", "lab_docs", "lab_solutions",
-              "labs", "reading")
-for (d in gen_dirs) {
-  files <- list.files(file.path("content", d), pattern = ".*\\.Rmd$", full.names = TRUE)
-  file.remove(files)
+  old_wd = getwd()
+  setwd(rprojroot::find_root(".semestr.here"))
+
+  gen_dirs <- c("assignment", "homework_solutions", "lab_docs", "lab_solutions",
+                "labs", "reading")
+  for (d in gen_dirs) {
+    fp <- file.path("content", d)
+    files <- list.files(fp, pattern = ".*\\.Rmd$", full.names = TRUE)
+    if (length(files) > 0) {
+      if (dry_run) {
+        message("would delete ", stringr::str_c(files, collapse = ", "))
+      } else {
+        if (verbose) {
+          message("deleting ", stringr::str_c(files, collapse = ", "))
+        }
+        file.remove("files")
+      }
+    } else {
+      if (verbose) {
+        message("nothing to delete in ", fp)
+      }
+    }
+  }
+
+  static_pdf_dirs <- c("lab_docs", "lab_doocs")
+
+  for (d in static_pdf_dirs) {
+    fp <- file.path("static", "files", d)
+    files <- list.files(fp, pattern = "\\.pdf$",
+                        full.names = TRUE, recursive = TRUE)
+    if (length(files) > 0) {
+      if (dry_run) {
+        message("would delete ", stringr::str_c(files, collapse = ", "))
+      } else {
+        if (verbose) {
+          message("deleting ", stringr::str_c(files, collapse = ", "))
+        }
+        file.remove("files")
+      }
+    } else {
+      if (verbose) {
+        message("nothing to delete in ", fp)
+      }
+    }
+  }
+
+  static_img_dirs <- c("lab_docs")
+
+  for (d in static_img_dirs) {
+    fp <- file.path("static", d)
+    files <- list.files(fp, pattern = "\\.(png|jpg|jpeg|svg|gif|pdf)$",
+                        full.names = TRUE, recursive = TRUE)
+    if (length(files) > 0) {
+      if (dry_run) {
+        message("would delete ", stringr::str_c(files, collapse = ", "))
+      } else {
+        if (verbose) {
+          message("deleting ", stringr::str_c(files, collapse = ", "))
+        }
+        file.remove("files")
+      }
+    } else {
+      if (verbose) {
+        message("nothing to delete in ", fp)
+      }
+    }
+  }
+
+  setwd(old_wd)
 }
-
-static_pdf_dirs <- c("lab_docs", "lab_doocs")
-
-for (d in static_dirs) {
-  files <- list.files(file.path("static", "files", d), pattern = "\\.pdf$",
-                      full.names = TRUE)
-  file.remove("files")
-}
-
-static_img_dirs <- c("lab_docs")
-
-for (d in static_dirs) {
-  files <- list.files(file.path("static", d),
-                      pattern = "\\.(png|jpg|jpeg|svg|gif|pdf)$",
-                      full.names = TRUE)
-  file.remove("files")
-}
-
-
-
-setwd(old_wd)
