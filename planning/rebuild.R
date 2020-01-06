f8bbba05d7e07adc962c63caaf1b6648c9c3fc8d
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2020-01-06 00:31:28 -0600

    Updated lab docs, fixing a couple of minor typos.

--- planning/rebuild.R
+++ planning/rebuild.R
@@ -1,5 +1,6 @@
 rebuild <- function() {
-  source('D:/JG_Documents/teaching/Classes_by_Semester/2019-2020/Spring/EES_3310/EES_3310_website/planning/init.R',
+  home <- rprojroot::find_root(".semestr.here")
+  source(file.path(home, 'planning/init.R'),
          chdir = TRUE, verbose = FALSE)
   semester <<- load_semester_db(file.path("planning", db_file))
   generate_assignments(semester)
