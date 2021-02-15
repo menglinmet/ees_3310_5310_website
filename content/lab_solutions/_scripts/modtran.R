6478c17b513e376c0782e9d45bd865b5e4ca986b
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2021-02-15 00:43:48 -0600

    * Slides for class #10. * Answers for Lab #2.

--- content/lab_solutions/_scripts/modtran.R
+++ content/lab_solutions/_scripts/modtran.R
@@ -179,6 +179,7 @@ run_modtran <- function(filename = NULL,
   }
   Sys.sleep(1)
 
+
   output <- str_c(body, collapse = "\n") %>% read_modtran(text = .)
   invisible(output)
 }
@@ -345,7 +346,7 @@ plot_modtran <- function(filename = NULL, text = NULL,
     ) %>% na.omit() %>% filter(between(k, k_limits[1], k_limits[2]))
 
   if (is.null(lambda)) {
-  lambda = c(1, 2, 2.5, 3, 3.5, 4, 5:10, 12, 14, 17, 20, 25, 30, 35, 40, 50, 100)
+    lambda = c(1, 2, 2.5, 3, 3.5, 4, 5:10, 12, 14, 17, 20, 25, 30, 35, 40, 50, 100)
   }
 
   spectrum <- spectrum %>% select(k, tk) %>% na.omit() %>%
