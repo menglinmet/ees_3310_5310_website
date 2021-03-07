1295b57ed1c26e7ee4d60a6959562073e3b38f4d
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2021-03-07 17:55:21 -0600

    Updated lab #7-9 instructions.

--- planning/regenerate_site.R
+++ planning/regenerate_site.R
@@ -13,6 +13,16 @@ regenerate_site <- function(root = NULL, force = FALSE) {
   message("Setting working directory to ", getwd())
   semester <- load_semester_db("planning/EES-3310-5310.sqlite3")
   generate_assignments(semester)
+  new_update_site(root = getwd(), force = force)
+}
+
+new_update_site <- function(root = NULL, force = FALSE) {
+  if (is.null(root)) {
+    root = find_root(criterion = has_file(".semestr.here"))
+  }
+  oldwd = setwd(root)
+  on.exit(setwd(oldwd))
+  message("Setting working directory to ", getwd())
   update_site(force = force)
   out_opts = list(md_extensions = semestr:::get_md_extensions(), toc = TRUE,
                   includes = list(in_header = "ees3310.sty"))
